4c9e331ab821a870c8d71ddfe88b6ce61de8dba4
minhdangnsp <dangnguyentranminh@gmail.com>
2022-10-17 16:57:46 +0700

    ch2.b5: Gọi hàm trong Event function của html

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,6 +6,10 @@ const TodoList = () => {
     //Hàm useState trả về 2 giá trị, 1 là tên biến, 2 là function để cập nhật lại biến này.
     //không nhất thiết phải đặt là setName, có thể đặt tên bất kỳ
     const [name, setName] = useState("")
+
+    const handleClickBtn = (event, msg) => {
+        console.log(">>run inside handleClickBtn ----:", name)
+    }
     return (
         <div>
             {/* từ khoá 'this' dùng trong trường hợp sử dụng class thì this sẽ hiểu là biến này dùng trong 
@@ -15,9 +19,12 @@ const TodoList = () => {
             trên ô input không thay đổi */}
             <input value={name} type="text"
                 onChange={(event) => {
-                    console.log("event: ", event)
                     setName(event.target.value)
                 }} />
+            {/* Cách 1 khi gọi hàm handleClickBtn đối với sự kiện onClick - dùng arrow function */}
+            <button type='button' onClick={() => handleClickBtn()}>Submit</button>
+            {/* Cách 2 khi gọi hàm handleClickBtn đối với sự kiện onClick - không dùng cặp dấu () */}
+            <button type='button' onClick={handleClickBtn}>Submit</button>
             <br /><br />
             Hello Todo List with name {name}
         </div>
